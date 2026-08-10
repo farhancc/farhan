8c0e5a47d3a0ce13c599c708249e7d763e89c3d6
farhan <farhancc123@gmail.com>
2026-08-10 15:26:14 +0530

    fix: replace Start button flag with pixel-perfect Windows XP 3D gradient flag

--- src/components/os/Taskbar.tsx
+++ src/components/os/Taskbar.tsx
@@ -20,12 +20,38 @@ export const Taskbar = () => {
           className="h-full pr-5 pl-2 italic font-bold text-white text-sm bg-gradient-to-b from-[#38A535] via-[#4EA52D] to-[#25821A] hover:brightness-110 active:brightness-90 rounded-r-2xl border-r border-[#ffffff4d] shadow-[3px_0_4px_rgba(0,0,0,0.4)] flex items-center shadow-[inset_2px_2px_4px_rgba(255,255,255,0.6)] z-50 overflow-hidden"
           style={{ borderRadius: '0 20px 20px 0' }}
         >
-          <div className="flex mr-1 items-center justify-center filter drop-shadow">
-             <svg width="22" height="22" viewBox="0 0 100 100" style={{ transform: 'perspective(100px) rotateY(-10deg) skewY(-5deg)' }}>
-               <path fill="#f05335" d="M10,25 C30,10 45,25 45,25 L45,45 C25,45 15,35 10,45 Z"/>
-               <path fill="#7abc15" d="M50,30 C70,15 90,30 90,30 L90,50 C70,50 60,40 50,50 Z"/>
-               <path fill="#029ced" d="M10,50 C30,35 45,50 45,50 L45,70 C25,70 15,60 10,70 Z"/>
-               <path fill="#ffb801" d="M50,55 C70,40 90,55 90,55 L90,75 C70,75 60,65 50,75 Z"/>
+          <div className="flex mr-1.5 items-center justify-center filter drop-shadow-[1px_1px_2px_rgba(0,0,0,0.6)]">
+             <svg width="20" height="20" viewBox="0 0 88 88" fill="none" xmlns="http://www.w3.org/2000/svg">
+               {/* Red Top-Left Pane */}
+               <path d="M10 20C24 11 38 19 42 17V40C38 42 24 34 10 43V20Z" fill="url(#xp-red)"/>
+               {/* Green Top-Right Pane */}
+               <path d="M46 17C50 19 64 11 78 20V43C64 34 50 42 46 40V17Z" fill="url(#xp-green)"/>
+               {/* Blue Bottom-Left Pane */}
+               <path d="M10 47C24 38 38 46 42 44V67C38 69 24 61 10 70V47Z" fill="url(#xp-blue)"/>
+               {/* Yellow Bottom-Right Pane */}
+               <path d="M46 44C50 46 64 38 78 47V70C64 61 50 69 46 67V44Z" fill="url(#xp-yellow)"/>
+               <defs>
+                 <linearGradient id="xp-red" x1="10" y1="17" x2="42" y2="43" gradientUnits="userSpaceOnUse">
+                   <stop stopColor="#FF6B4A"/>
+                   <stop offset="0.6" stopColor="#EE3816"/>
+                   <stop offset="1" stopColor="#B31A00"/>
+                 </linearGradient>
+                 <linearGradient id="xp-green" x1="46" y1="17" x2="78" y2="43" gradientUnits="userSpaceOnUse">
+                   <stop stopColor="#9BD835"/>
+                   <stop offset="0.6" stopColor="#70B814"/>
+                   <stop offset="1" stopColor="#448000"/>
+                 </linearGradient>
+                 <linearGradient id="xp-blue" x1="10" y1="44" x2="42" y2="70" gradientUnits="userSpaceOnUse">
+                   <stop stopColor="#3FA5F6"/>
+                   <stop offset="0.6" stopColor="#0072E3"/>
+                   <stop offset="1" stopColor="#0043A4"/>
+                 </linearGradient>
+                 <linearGradient id="xp-yellow" x1="46" y1="44" x2="78" y2="70" gradientUnits="userSpaceOnUse">
+                   <stop stopColor="#FFDE43"/>
+                   <stop offset="0.6" stopColor="#F5B400"/>
+                   <stop offset="1" stopColor="#C68200"/>
+                 </linearGradient>
+               </defs>
              </svg>
           </div>
           <span className="text-[17px] drop-shadow-[1px_1px_3px_rgba(0,0,0,0.8)] ml-1 font-sans tracking-wide pb-0.5">start</span>
