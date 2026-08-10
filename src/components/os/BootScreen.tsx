dd4327cc2c0db32615ef1aa8abb7daf9aea31bbe
farhan <farhancc123@gmail.com>
2026-08-10 15:31:16 +0530

    fix: replace BootScreen flat Windows logo with 3D gradient Windows XP flag SVG and drop shadow glow

--- src/components/os/BootScreen.tsx
+++ src/components/os/BootScreen.tsx
@@ -15,19 +15,45 @@ export const BootScreen = () => {
   return (
     <div className="fixed inset-0 z-[10000] bg-black flex flex-col items-center justify-center select-none cursor-none">
       <div className="flex flex-col items-center mb-16 scale-125">
-        <div className="flex mb-2">
-           <svg width="60" height="60" viewBox="0 0 100 100">
-             <path fill="#f05335" d="M10,25 C30,10 45,25 45,25 L45,45 C25,45 15,35 10,45 Z"/>
-             <path fill="#7abc15" d="M50,30 C70,15 90,30 90,30 L90,50 C70,50 60,40 50,50 Z"/>
-             <path fill="#029ced" d="M10,50 C30,35 45,50 45,50 L45,70 C25,70 15,60 10,70 Z"/>
-             <path fill="#ffb801" d="M50,55 C70,40 90,55 90,55 L90,75 C70,75 60,65 50,75 Z"/>
+        <div className="flex mb-3">
+           <svg width="72" height="72" viewBox="0 0 88 88" fill="none" xmlns="http://www.w3.org/2000/svg" className="drop-shadow-[0_0_15px_rgba(255,255,255,0.4)]">
+             {/* Red Top-Left Pane */}
+             <path d="M10 20C24 11 38 19 42 17V40C38 42 24 34 10 43V20Z" fill="url(#boot-red)"/>
+             {/* Green Top-Right Pane */}
+             <path d="M46 17C50 19 64 11 78 20V43C64 34 50 42 46 40V17Z" fill="url(#boot-green)"/>
+             {/* Blue Bottom-Left Pane */}
+             <path d="M10 47C24 38 38 46 42 44V67C38 69 24 61 10 70V47Z" fill="url(#boot-blue)"/>
+             {/* Yellow Bottom-Right Pane */}
+             <path d="M46 44C50 46 64 38 78 47V70C64 61 50 69 46 67V44Z" fill="url(#boot-yellow)"/>
+             <defs>
+               <linearGradient id="boot-red" x1="10" y1="17" x2="42" y2="43" gradientUnits="userSpaceOnUse">
+                 <stop stopColor="#FF6B4A"/>
+                 <stop offset="0.6" stopColor="#EE3816"/>
+                 <stop offset="1" stopColor="#B31A00"/>
+               </linearGradient>
+               <linearGradient id="boot-green" x1="46" y1="17" x2="78" y2="43" gradientUnits="userSpaceOnUse">
+                 <stop stopColor="#9BD835"/>
+                 <stop offset="0.6" stopColor="#70B814"/>
+                 <stop offset="1" stopColor="#448000"/>
+               </linearGradient>
+               <linearGradient id="boot-blue" x1="10" y1="44" x2="42" y2="70" gradientUnits="userSpaceOnUse">
+                 <stop stopColor="#3FA5F6"/>
+                 <stop offset="0.6" stopColor="#0072E3"/>
+                 <stop offset="1" stopColor="#0043A4"/>
+               </linearGradient>
+               <linearGradient id="boot-yellow" x1="46" y1="44" x2="78" y2="70" gradientUnits="userSpaceOnUse">
+                 <stop stopColor="#FFDE43"/>
+                 <stop offset="0.6" stopColor="#F5B400"/>
+                 <stop offset="1" stopColor="#C68200"/>
+               </linearGradient>
+             </defs>
            </svg>
         </div>
-        <div className="text-white text-3xl font-bold flex items-end">
-          Microsoft<span className="text-4xl ml-2 font-black italic">Windows</span>
-          <span className="text-[#f18e1c] text-xs font-black pb-1 ml-1">XP</span>
+        <div className="text-white text-3xl font-bold flex items-end tracking-tight font-sans drop-shadow-md">
+          Microsoft<span className="text-4xl ml-2 font-black italic tracking-tighter">Windows</span>
+          <span className="text-[#f18e1c] text-xs font-black pb-1.5 ml-1 tracking-widest">XP</span>
         </div>
-        <div className="w-full h-[1px] bg-gradient-to-r from-transparent via-gray-500 to-transparent mt-1" />
+        <div className="w-full h-[1px] bg-gradient-to-r from-transparent via-gray-600 to-transparent mt-2" />
       </div>
 
       {/* Loading Bar Container */}
